49d2eee8e91f42e74d9e2fae67057b1b08769bc5
CAUADEV <cauadev@gmail.com>
2021-05-26 17:10:52 -0300

    implementando exercicio frontend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,11 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    document.querySelector('html').style.background = 'linear-gradient(to right, #f64f59, #c471ed, #12c2e9)'
+  }
+
+  ngOnDestroy(): void{
+    document.querySelector('html').style.background = 'none'
   }
 
 }
